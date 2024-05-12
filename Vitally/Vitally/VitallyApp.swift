15b5b66402279f610e80967f995744f5e8ecc919
andrewaustin12 <andrew.d.austin2@gmail.com>
2024-05-12 10:19:02 +0200

    firist

---
+++ Vitally/Vitally/VitallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitallyApp.swift
+//  Vitally
+//
+//  Created by andrew austin on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct VitallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
